2624a11dc2dd7c9c298b761788ab56278abb3df4
Danica Fisher <danicafisher@users.noreply.huggingface.co>
2024-10-15 16:11:50 +0000

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import PyMuPDFLoader, TextLoader
+from langchain_community.vectorstores import Qdrant
+
+def process_file(file):
+    documents = []
+    if file.endswith(".pdf"):
+        loader = PyMuPDFLoader(file)
+        docs = loader.load()
+        documents.extend(docs)
+    else:
+        loader = TextLoader(file)
+        docs = loader.load()
+        documents.extend(docs)
+    return documents
+
+
+def add_to_qdrant(documents, embeddings, qdrant_client, collection_name):
+    
+    Qdrant.from_documents(
+        documents,
+        embeddings,
+        url=qdrant_client.url,
+        prefer_grpc=True,
+        collection_name=collection_name,
+    )
