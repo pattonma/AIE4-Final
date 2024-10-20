5f343ce95422a7072c13e0c1d721fa5348f58178
pattonma <pattonma22@gmail.com>
2024-10-19 22:19:28 -0400

    reqs

--- constants.py
+++ constants.py
@@ -10,3 +10,4 @@ LANGCHAIN_ENDPOINT='https://api.smith.langchain.com'
 QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
 QDRANT_ENDPOINT = os.environ.get("QDRANT_ENDPOINT")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
+COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
